029322dc4752140d6065d1a8ba2ad15d80ececaa
Kaylin Bittner <39201191+kaylinbittner@users.noreply.github.com>
2019-04-18 15:44:33 +0200

    Update basics4.go

--- basics/basics4.go
+++ basics/basics4.go
@@ -7,6 +7,10 @@ func add(x int, y int) int {
 }
 
 // int is the variable type
+// if both arguments are the same type, only the last one requires a declaration
+// i.e. func add(x, y int) int {
+
+
 
 func main() {
 	fmt.Println(add(42, 13))
